8f8a59ac7929fe3ed9491b8b4eda48e5b546a320
Nasreddine <nasreddine@MacBook-Pro-de-Nasreddine.local>
2020-10-16 13:54:38 +0200

    form component

--- shared/components/form/form.component.ts
+++ shared/components/form/form.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'form-component',
   templateUrl: './form.component.html',
   styleUrls: ['./form.component.scss']
 })
-export class FormComponent {
-  constructor() {}
+export class FormComponent implements OnInit {
+  form: FormGroup;
+  @Input() inputsList: string[];
+  @Input() label: string;
+  @Output() send = new EventEmitter();
+
+  constructor(public builder: FormBuilder) {}
+
+  ngOnInit(): void {
+    this.inputsList = Object.keys({ name: '', port: '', url: '' });
+  }
+
+  onSend(): void {
+    this.send.emit(this.form.value);
+  }
 }
