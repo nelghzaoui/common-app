9f6e3b4cbf1da696570eb43a574867ff539c112b
Nasreddine <eln@outlook.be>
2020-11-06 00:16:46 +0100

    add models folder in shared module

---
+++ shared/models/index.ts
@@ -0,0 +1,4 @@
+import { Item } from './item.interface';
+import { ListAction } from './list-action.enum';
+
+export { Item, ListAction };
---
+++ shared/models/list-action.enum.ts
@@ -0,0 +1,6 @@
+export enum ListAction {
+  CREATE = 'CREATE',
+  READ = 'READ',
+  UPDATE = 'UPDATE',
+  DELETE = 'DELETE'
+}
