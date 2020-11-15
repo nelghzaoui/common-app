6230045e0200baf0bef8ff964ba54a029c0157af
Nasreddine <eln@outlook.be>
2020-11-15 06:03:21 +0100

    use toolbar-layout in form page + remove fab button

--- shared/shared.module.ts
+++ shared/shared.module.ts
@@ -1,15 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
 import { TranslateModule } from '@ngx-translate/core';
 import { COMPONENTS } from './components';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { LAYOUT } from './layout';
 
 export const MODULES = [CommonModule, FormsModule, IonicModule, ReactiveFormsModule, TranslateModule];
 
 @NgModule({
-  declarations: [COMPONENTS],
+  declarations: [COMPONENTS, LAYOUT],
   imports: [MODULES],
-  exports: [MODULES, COMPONENTS]
+  exports: [MODULES, COMPONENTS, LAYOUT]
 })
 export class SharedModule {}
