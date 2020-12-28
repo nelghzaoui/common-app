08c761783e6cca9acf0e807c149b32940838896d
Nasreddine <eln@outlook.be>
2020-12-28 20:03:53 +0100

    finalize home page

--- features/account/pages/home/home.page.ts
+++ features/account/pages/home/home.page.ts
@@ -1,11 +1,19 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { NavController } from '@ionic/angular';
 
 @Component({
   templateUrl: './home.page.html',
   styleUrls: ['./home.page.scss']
 })
 export class HomePage implements OnInit {
-  constructor() {}
+  constructor(private readonly navCtrl: NavController, private readonly route: ActivatedRoute) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    //TODO: Should not appear if the user has already an account
+  }
+
+  onClick(): void {
+    this.navCtrl.navigateForward(['register'], { relativeTo: this.route });
+  }
 }
