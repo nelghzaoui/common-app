3ee15e21dd972a30bd234443913bd8affcdd270d
Nasreddine <eln@outlook.be>
2021-01-20 20:28:44 +0100

    add alert service and declare everything in alert module

--- features/alert/alert.module.ts
+++ features/alert/alert.module.ts
@@ -1,7 +1,8 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { SharedModule } from '@shared/shared.module';
-import { ListPage } from './list/list.page';
+import { ListPage } from './pages';
+import { AlertService } from './services/alert/alert.service';
 
 export const ROUTES: Routes = [
   {
@@ -11,6 +12,7 @@ export const ROUTES: Routes = [
 ];
 @NgModule({
   declarations: [ListPage],
-  imports: [SharedModule, RouterModule.forChild(ROUTES)]
+  imports: [SharedModule, RouterModule.forChild(ROUTES)],
+  providers: [AlertService]
 })
 export class AlertModule {}
---
+++ features/alert/models/alert.class.ts
@@ -0,0 +1,14 @@
+import { AlertPriority } from './priority.enum';
+import { AlertType } from './type.enum';
+
+export class Alert {
+  constructor(
+    public name: string,
+    public location: string,
+    public date: Date,
+    public type: AlertType,
+    public priority: AlertPriority,
+    public reporter: string,
+    public assignee: string
+  ) {}
+}
